a2059b4db5a247abbea898402a74c8174048c565
George Joubran <durd2001@gmail.com>
2021-11-03 16:29:27 +0200

    Closing touches

--- handlers/data.js
+++ handlers/data.js
@@ -1,6 +1,5 @@
 const fs = require("fs");
 const path = require("path");
-const autoComplete = require("./autoComplete");
 
 function dataHandler(request, response) {
   const filePath = path.join(__dirname, "..", "data", "pokemons.json");
@@ -10,21 +9,17 @@ function dataHandler(request, response) {
       respond.writeHead(500, { "content-type": "application/json" });
       response.end({});
     } else {
-      response.writeHead(200, { "content-type": "application/json" });
-      console.log(request.url);
+
       const data = new URLSearchParams(request.url.split("?")[1]);
       const query = data.get("q");
-      console.log("query: " + query);
       let pokemons = [];
       let res = JSON.parse(file);
-      pokemons = res.results.filter(
-        (element) => element.name.indexOf(query) == 0
-      );
-      for (let i = 0; i < pokemons.length; i++) {
-        if (pokemons[i].name == query) pokemons.splice(i, 1);
-        console.log(pokemons[i]);
-      }
 
+      pokemons = res.results.filter(
+        (element) => element.name.indexOf(query.toLowerCase()) == 0
+      ).slice(0, 10);
+
+      response.writeHead(200, { "content-type": "application/json" });
       response.end(JSON.stringify(pokemons));
     }
   });
--- www/script.js
+++ www/script.js
@@ -1,34 +1,61 @@
+//pokemon search div
 const input = document.querySelector(".pokemonSearch");
 const results = document.querySelector(".pokemonResults");
 const resultsDiv = document.querySelector(".pokemonResults");
 //search bar animations
 const pokemonInput = document.querySelector(".pokemonSearch");
 const animationButton = document.querySelector("#search");
-
+//pokemon info div
+const card = document.querySelector(".card");
+const img = document.querySelector(".pokemonImg");
+const allTd = document.querySelectorAll("td");
+const pokeName = document.querySelector(".name");
+const pokeTypes = document.querySelector(".types");
+const pokeHeight = document.querySelector(".height");
+const pokeWeight = document.querySelector(".weight");
+const pokeAbilities = document.querySelector(".abilities");
 input.addEventListener("keyup", (event) => {
   fetch("/data?q=" + input.value)
     .then((response) => {
       if (!response.ok) throw new Error(response.status);
       return response.json();
     })
-    .then((json) => {
+    .then((pokemon) => {
       resultsDiv.innerHTML = "";
-      let length = 10;
-      if (json.length < 10) length = json.length;
-      for (let i = 0; i < length; i++) {
+      for (let i = 0; i < pokemon.length; i++) {
         let span = document.createElement("span");
         let br = document.createElement("br");
-        span.textContent = json[i].name;
+        span.textContent = formatString(pokemon[i].name);
+        span.style.cursor = "pointer";
         resultsDiv.appendChild(span);
         resultsDiv.appendChild(br);
 
         span.addEventListener("click", (event) => {
           input.value = span.textContent;
           resultsDiv.innerHTML = "";
+          fetch(pokemon[i].url).then((response) => {
+            if (!response.ok) throw new Error(response.status);
+            return response.json();
+          }).then(pokeData => {
+            img.src = pokeData.sprites.front_default;
+            pokeName.textContent = formatString(pokeData.name);
+            pokeTypes.textContent = formatString(pokeData.types[0].type.name)
+            pokeHeight.textContent = "Height: " + pokeData.height / 10.0 + "m";
+            pokeWeight.textContent = "Weight: " + pokeData.weight / 10.0 + "kg"
+            pokeAbilities.textContent = "Abilities: " + formatString(pokeData.abilities[0].ability.name) + "/" + formatString(pokeData.abilities[1].ability.name)
+
+            if (pokeData.types.length == 2) pokeTypes.textContent += "/" + formatString(pokeData.types[1].type.name)
+            allTd.forEach((element, index) => {
+              element.textContent = pokeData.stats[index].base_stat;
+            })
+          }).catch((error) => console.error(error));
+          card.style.display = "inline-block"
         });
       }
-      if (!input.value.length) resultsDiv.innerHTML = "";
-      console.log(json.length);
+      if (!input.value.length) {
+        resultsDiv.innerHTML = "";
+        card.style.display = "none"
+      }
     })
     .catch((error) => console.error(error));
 });
@@ -36,8 +63,20 @@ input.addEventListener("keyup", (event) => {
 //animations for the search button
 pokemonInput.addEventListener("click", () => {
   pokemonInput.classList.add("testClass");
+  resultsDiv.classList.remove("hideResults")
 });
 
 animationButton.addEventListener("click", () => {
   pokemonInput.classList.remove("testClass");
+  resultsDiv.classList.add("hideResults")
+  card.style.display = "none"
 });
+//pokemon-data => ["pokemon","data"] => "pokemon data"
+
+const formatString = (string) => {
+  let strArray = [];
+  string.split("-").forEach((word, index) => {
+    strArray[index] = word[0].toUpperCase() + word.slice(1, word.length);
+  });
+  return strArray.join(' ');
+}
