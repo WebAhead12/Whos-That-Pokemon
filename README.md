5c4e0f6983a5626d87927a7660ca205fbd84054b
George Joubran <durd2001@gmail.com>
2021-11-04 10:46:36 +0200

    Resolving Issues #1 #2 #3 #4 and #7

---
+++ README.md
@@ -0,0 +1,19 @@
+# Whos That Pokemon
+A website which allows you to search for pokemon data
+
+# Features
+- Auto complete while searching<br>
+- Pokemon name<br>
+- Pokemon type<br>
+- Pokemon stats<br>
+
+# Work Distribution
+
+script.js: George + Mohammad <br>
+CSS: Zahi <br> 
+index.html: Zahi + George <br>
+autocomplete: George <br>
+data.js: Mohammad <br>
+
+# Website
+[https://whosthattpokemon.herokuapp.com/](https://whosthattpokemon.herokuapp.com/)
--- www/script.js
+++ www/script.js
@@ -33,6 +33,7 @@ const typeColors = {
   ghost: (a = 1) => `rgba(112, 88, 152,${a})`,
   ice: (a = 1) => `rgba(152, 216, 216,${a})`,
   dragon: (a = 1) => `rgba(112, 56, 248,${a})`,
+  flying: (a = 1) => `rgba(255, 255, 255,${a})`,
 };
 
 input.addEventListener("keyup", (event) => {
@@ -43,46 +44,21 @@ input.addEventListener("keyup", (event) => {
     })
     .then((pokemon) => {
       resultsDiv.innerHTML = "";
-      for (let i = 0; i < pokemon.length; i++) {
-        let span = document.createElement("span");
-        let br = document.createElement("br");
-        span.textContent = formatString(pokemon[i].name);
-        span.style.cursor = "pointer";
-        resultsDiv.appendChild(span);
-        resultsDiv.appendChild(br);
+      pokemon.forEach((poke) => {
+        let searchResult = document.createElement("span");
+        let emptybreak = document.createElement("br");
+        searchResult.textContent = formatString(poke.name);
+        searchResult.style.cursor = "pointer";
+        resultsDiv.appendChild(searchResult);
+        resultsDiv.appendChild(emptybreak);
 
-        span.addEventListener("click", (event) => {
-          input.value = span.textContent;
+        searchResult.addEventListener("click", (event) => {
+          input.value = searchResult.textContent;
           resultsDiv.innerHTML = "";
-          fetch(pokemon[i].url).then((response) => {
-            if (!response.ok) throw new Error(response.status);
-            return response.json();
-          }).then(pokeData => {
-            img.src = pokeData.sprites.front_default;
-            pokeName.textContent = formatString(pokeData.name);
-            pokeHeight.textContent = "Height: " + pokeData.height / 10.0 + "m";
-            pokeWeight.textContent = "Weight: " + pokeData.weight / 10.0 + "kg"
-
-            pokeAbilities.textContent = "Abilities: " + formatString(pokeData.abilities[0].ability.name) + "/" + formatString(pokeData.abilities[1].ability.name)
-
-            // pokeTypes.textContent = formatString(pokeData.types[0].type.name)
-            // if (pokeData.types.length == 2) pokeTypes.textContent += "/" + formatString(pokeData.types[1].type.name)
-
-            pokeType1.textContent = formatString(pokeData.types[0].type.name)
-            pokeType1.style.backgroundColor = typeColors[pokeData.types[0].type.name](0.5);
-            pokeType1.style.borderColor = typeColors[pokeData.types[0].type.name]();
-            if (pokeData.types.length == 2) {
-              pokeType2.textContent = " " + formatString(pokeData.types[1].type.name)
-              pokeType2.style.backgroundColor = typeColors[pokeData.types[1].type.name](0.5);
-              pokeType2.style.borderColor = typeColors[pokeData.types[1].type.name]();
-            }
-            allTd.forEach((element, index) => {
-              element.textContent = pokeData.stats[index].base_stat;
-            })
-          }).catch((error) => console.error(error));
+          fetchData(poke.url)
           card.style.display = "inline-block"
         });
-      }
+      })
       if (!input.value.length) {
         resultsDiv.innerHTML = "";
         card.style.display = "none"
@@ -96,6 +72,8 @@ pokemonInput.addEventListener("click", () => {
   pokemonInput.classList.add("testClass");
   resultsDiv.classList.remove("hideResults")
   card.style.display = "none"
+  pokeType1.innerHTML = " "
+  pokeType2.innerHTML = " "
 });
 
 animationButton.addEventListener("click", () => {
@@ -108,7 +86,41 @@ animationButton.addEventListener("click", () => {
 const formatString = (string) => {
   let strArray = [];
   string.split("-").forEach((word, index) => {
-    strArray[index] = word[0].toUpperCase() + word.slice(1, word.length);
+    strArray[index] = word[0].toUpperCase() + word.slice(1);
   });
   return strArray.join(' ');
+}
+
+const fetchData = (url) => {
+  fetch(url).then((response) => {
+    if (!response.ok) throw new Error(response.status);
+    return response.json();
+  }).then(pokeData => {
+    img.src = pokeData.sprites.front_default;
+    pokeName.textContent = formatString(pokeData.name);
+    pokeHeight.textContent = "Height: " + pokeData.height / 10.0 + "m";
+    pokeWeight.textContent = "Weight: " + pokeData.weight / 10.0 + "kg"
+
+    if (pokeData.abilities.length != 0) {
+      pokeAbilities.textContent = "Abilities: " + formatString(pokeData.abilities[0].ability.name);
+      if (pokeData.abilities.length == 2) {
+        pokeAbilities.textContent += "/" + formatString(pokeData.abilities[1].ability.name)
+      }
+    }
+
+    console.log(formatString(pokeData.types[0].type.name))
+    pokeType1.textContent = formatString(pokeData.types[0].type.name)
+    pokeType1.style.backgroundColor = typeColors[pokeData.types[0].type.name](0.5);
+    pokeType1.style.borderColor = typeColors[pokeData.types[0].type.name]();
+    if (pokeData.types.length > 1) {
+      pokeType2.textContent = " " + formatString(pokeData.types[1].type.name)
+      pokeType2.style.backgroundColor = typeColors[pokeData.types[1].type.name](0.5);
+      pokeType2.style.borderColor = typeColors[pokeData.types[1].type.name]();
+      pokeType2.style.display = "inline-block";
+    } else pokeType2.style.display = "none";
+
+    allTd.forEach((element, index) => {
+      element.textContent = pokeData.stats[index].base_stat;
+    })
+  }).catch((error) => console.error(error));
 }
